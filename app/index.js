5fa54e0f4ee1e43dee049d7d41b9338cffceb050
Anders Wood <anderswood@gmail.com>
2017-05-17 12:56:04 -0600

    connectedRouter is screwing it up

--- app/index.js
+++ app/index.js
@@ -1,26 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { App } from './components/app';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
-import { loadMovies } from './actions/index.js';
-import MovieContainer from './containers/movies-container.js'
+import { Provider } from 'react-redux';
+import { ConnectedRouter, routerReducer, routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router-dom';
 
+// import MovieContainer from './containers/movies-container.js'
 // import MovieIndex from './components/movieIndex';
-import { BrowserRouter as Router } from 'react-router-dom';
+import App from './components/app';
+import { loadMovies } from './actions/index.js';
 import { movies } from './reducers/movie-reducer.js';
-import Provider from 'react-redux';
+
+const history = createHistory()
+const routeMiddleware = routerMiddleware(history)
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
-const store = createStore(movies, devTools, applyMiddleware(thunk))
+const store = createStore(combineReducers({
+  movies,
+  router: routerReducer}), devTools, applyMiddleware(thunk, routeMiddleware))
+
 store.dispatch(loadMovies())
 // console.log(store);
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router >
-      <MovieContainer />
-    </Router>
+    <ConnectedRouter history={ history }>
+      <Route  path='/'
+              component={ App }>
+      </Route>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById('main'))
+
+
+
+  // <Provider store={ store }>
+  //   <ConnectedRouter history={ history } >
+  //     <Route  path='/'
+  //             component={ App }/>
+  //   </ConnectedRouter>
+  // </Provider>,
+  // <App/>,

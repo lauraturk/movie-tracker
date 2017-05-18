1cc827d1de9cc7b961d537fc3532a0789dcd6780
Laura Turk <laura.lk.turk@gmail.com>
2017-05-18 15:17:38 -0600

    sign in and create a new user

--- app/components/UserSignIn.js
+++ app/components/UserSignIn.js
@@ -12,40 +12,50 @@ class UserSignIn extends Component{
   }
 
   handleNewUser() {
-    const newUserBody = {
-      name: 'bob', password: 'password', email: 'bob@gmail.com'
-    }
+    this.apiFetch('/api/users/new', this.state)
+    .then(responseId => this.handleSignInUser())
+    .catch(error => console.log(error, "new user error"))
+  }
 
-    const signInBody = {
-      email: 'bob@gmail.com', password: 'password'
-    }
-
-    const newUserApi = '/api/users/new'
-    const signInApi = '/api/users/'
-
-    let { handleSignIn, userId } = this.props
-
-    fetch(signInApi, {
-      method: 'POST',
-      body: JSON.stringify(signInBody),
-      headers: { 'Content-Type':'application/json' },
-    })
-    .then(response => response.json())
+  handleSignInUser() {
+    let { handleSignIn } = this.props
+    this.apiFetch('/api/users/', {email: this.state.email, password: this.state.password})
     .then(responseId => handleSignIn(responseId.data))
-    .catch(error => console.log(error))
+    .catch(error => console.log(error, "handleSignInUser error"))
   }
 
 
+
+  apiFetch(fetchType, body) {
+    return fetch(fetchType, {
+      method: 'POST',
+      body: JSON.stringify(body),
+      headers: { 'Content-Type':'application/json' },
+    })
+    .then(response => response.json())
+  }
 
   render() {
     let { userId } = this.props;
     return(
       <div>
-        <form onClick={ e => {
-          e.preventDefault()
-          this.handleNewUser()
-        }}>
+        <form>
+          <input  value={this.state.email}
+                  placeholder="email"
+                  onChange={ e => this.setState({ email: e.target.value} )} />
+          <input  value={this.state.password}
+                  placeholder="password"
+                  onChange={ e => this.setState({ password: e.target.value} )} />
 
+          <input type="submit" placeholder="Sign In" onClick={ e => {
+            e.preventDefault()
+            this.handleSignInUser()
+
+          }}/>
+
+        </form>
+
+        <form>
           <input  value={this.state.name}
                   placeholder="Name"
                   onChange={ e => this.setState({ name: e.target.value} )} />
@@ -56,8 +66,10 @@ class UserSignIn extends Component{
                   placeholder="password"
                   onChange={ e => this.setState({ password: e.target.value} )} />
 
-          <button>Sign In</button>
-
+          <input type="submit" placeholder="Create Account" onClick={ e => {
+            e.preventDefault()
+            this.handleNewUser()
+          }}/>
         </form>
       </div>
     )

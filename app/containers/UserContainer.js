c7aad7e20b14ee946d0fb14f19a505f86144ad14
Travis <tlgreg86@gmail.com>
2017-05-18 16:48:45 -0600

    Remove exact route

--- app/containers/UserContainer.js
+++ app/containers/UserContainer.js
@@ -3,12 +3,7 @@ import { signInUser } from '../actions/index.js';
 import UserSignIn from '../components/UserSignIn.js';
 
   const mapStateToProps = (state) => {
-    // console.log(state)
-    if(!state.user){
-      alert('you piece of poop, sign in!')
-    } else {
-      return { userId : state.user}
-    }
+    return { userId : state.user}
   }
 
   const mapDispatchToProps = (dispatch) => {

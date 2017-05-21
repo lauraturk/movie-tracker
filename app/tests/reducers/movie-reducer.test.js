0a439a06d1e16ab9e7981f96c1c1ce731796b663
Travis <tlgreg86@gmail.com>
2017-05-21 11:17:55 -0600

    Add last of actions tests, add movie reducer test and begin user reducer test

---
+++ app/tests/reducers/movie-reducer.test.js
@@ -0,0 +1,31 @@
+import {movies} from '../../reducers/movie-reducer';
+
+const moviesArray = [{
+  movie_id: 283995,
+  title: "Guardians of the Galaxy Vol. 2",
+  poster_path: "/y4MBh0EjBlMuOzv9axM4qJlmhzz.jpg",
+  release_date: "2017-04-24",
+  vote_average: 7.6,
+  overview: "Great Movie",
+  fav_id: null
+}]
+
+describe('movie reducer tests', () => {
+
+
+  it('should return an empty array', () => {
+
+    expect(movies(undefined, {})).toEqual([])
+  })
+
+  it('should add all of the movies', () => {
+
+    const action = {
+      type: 'ADD_MOVIES',
+      moviesArray: moviesArray
+    }
+
+    expect(movies(undefined, action)).toHaveLength(1)
+    expect(movies([ action ], action)).toHaveLength(2)
+  })
+})

fc839477fdd9b027c635fd4843792dcadaafd88a
Anders Wood <anderswood@gmail.com>
2017-05-21 11:13:11 -0600

    fix favorites history

--- app/components/UserSignIn.js
+++ app/components/UserSignIn.js
@@ -22,13 +22,11 @@ class UserSignIn extends Component{
   handleSignInUser() {
     let { handleSignIn, loadUserFavorites, history } = this.props
 
-    ApiCalls.logInFetch('/api/users/',
-      {email: this.state.email, password: this.state.password}
-    )
+    ApiCalls.logInFetch(this.state.email, this.state.password)
     .then(responseId => {
       handleSignIn(responseId.data)
-      this.retrieveFavs(responseId.data)
       history.history.replace('/')
+      this.retrieveFavs(responseId.data)
     })
     .then(loadUserFavorites())
     .catch(error => console.log(error, "handleSignInUser error"))
@@ -37,35 +35,17 @@ class UserSignIn extends Component{
 
   retrieveFavs(userData) {
     let { loadUserFavorites } = this.props
-
-    fetch(`api/users/${userData.id}/favorites/`)
+    console.log('userId logging in: ', userData.id);
+    fetch(`api/users/${userData.id}/favorites`)
     .then(response => response.json())
-    .then(favArrObj => {loadUserFavorites(favArrObj.data)})
+    .then(favArrObj => {
+      loadUserFavorites(favArrObj.data)
+    })
     .catch(error => console.log('retrieve favorites error: ', error))
   }
 
   render() {
     let { userId, handleSignOut, clearAllFavorites } = this.props;
-
-      // const modalStyle = {
-      //   position: 'absolute',
-      //   top: '50%',
-      //   left: '50%',
-      //   transform: 'translate(-50%, -50%)',
-      //   zIndex: '9999',
-      //   background: '#fffF'
-      // }
-
-      // const backdropStyle = {
-      //   position: 'absolute',
-      //   width: '100%',
-      //   height: '100%',
-      //   top: '0px',
-      //   left: '0px',
-      //   zIndex: '9998',
-      //   background: 'rgba(0, 0, 0, 0.3)'
-      // }
-
 
     const newUserSignInForm = () => {
       return(
